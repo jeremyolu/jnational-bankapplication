1a1e32b20ab024d4b53b085c9b4230b18bebd99f
Jeremy Olu <jeremy-olu@hotmail.com>
2019-12-28 23:37:18 +0000

    added unit test configuration

---
+++ JNationalBankApplicationTests/NoMockingTests/CustomerServiceTests/CustomerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace JNationalBankApplicationTests.NoMockingTests.CustomerServiceTests
+{
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        public void DisplayBalanceStatus_BalanceLessThan0_ShouldReturnMessage()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ JNationalBankApplicationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JNationalBankApplicationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JNationalBankApplicationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4710eac0-77d7-43e9-8140-712cf81f9a6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
