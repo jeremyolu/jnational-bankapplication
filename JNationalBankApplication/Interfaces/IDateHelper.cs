b7a28c20bf3d3c3a19407857c55d54d4cc46da9e
Jeremy Olu <jeremy-olu@hotmail.com>
2019-12-30 15:37:55 +0000

    added extention methods for dates

--- JNationalBankApplication/Interfaces/IDateHelper.cs
+++ JNationalBankApplication/Interfaces/IDateHelper.cs
@@ -1,7 +1,15 @@
-﻿namespace JNationalBankApplication.Interfaces
+﻿using System;
+
+namespace JNationalBankApplication.Interfaces
 {
     public interface IDateHelper
     {
+        DateTime GetCurrentDateTime();
 
+        DateTime SetDays(DateTime date, int days);
+
+        DateTime SetMonths(DateTime date, int days);
+
+        DateTime SetYears(DateTime date, int days);
     }
 }
--- JNationalBankApplication/Utilities/DateHelper.cs
+++ JNationalBankApplication/Utilities/DateHelper.cs
@@ -1,9 +1,28 @@
 ﻿using JNationalBankApplication.Interfaces;
+using System;
 
 namespace JNationalBankApplication.Utilities
 {
     public class DateHelper : IDateHelper
     {
+        public DateTime GetCurrentDateTime()
+        {
+            return DateTime.Now;
+        }
 
+        public DateTime SetDays(DateTime date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        public DateTime SetMonths(DateTime date, int months)
+        {
+            return date.AddMonths(months);
+        }
+
+        public DateTime SetYears(DateTime date, int years)
+        {
+            return date.AddMonths(years);
+        }
     }
 }
